46fdb3376b5bec090061ebd46471021b4f6ae795
Tatyana Olshanskaya <pandochka2222@mail.ru>
2018-05-20 17:26:15 +0300

    Instal

---
+++ server.js
@@ -0,0 +1,20 @@
+
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(request, response){
+	console.log(request.method, request.url);
+	if(request.url == '/style.css'){
+		const css = fs.readFileSync('style.css','utf8');
+		response.end(css);
+	} else {
+	const html = fs.readFileSync('index.html','utf8');
+	response.end(html);
+	}	
+});
+
+console.log('port =', process.env.PORT);
+
+server.listen(process.env.PORT || 3000);
+console.log('Server started');
